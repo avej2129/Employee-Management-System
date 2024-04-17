cef63a4eeae7820391c4462a4e5af0d54bb3b4cc
Avej <avejshaikh018@gmail.com>
2024-04-17 13:07:38 +0530

    new method added

--- Employee-Management-System/src/main/java/com/employees/application/controller/Controller.java
+++ Employee-Management-System/src/main/java/com/employees/application/controller/Controller.java
@@ -1,4 +1,17 @@
 package com.employees.application.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
+
+    @GetMapping("/hi")
+    public String hello(){
+        return"hello" ;
+    }
+    @GetMapping("/hello")
+    public String HelloWorld(){
+        return "hello World";
+    }
 }
