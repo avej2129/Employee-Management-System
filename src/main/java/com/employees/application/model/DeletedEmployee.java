586e330b7b742c1e7b0b0e4dd3c52618c6f94ef5
Avej <avejshaikh018@gmail.com>
2024-11-11 15:46:36 +0530

    initial commit

--- src/main/java/com/employees/application/model/DeletedEmployee.java
+++ src/main/java/com/employees/application/model/DeletedEmployee.java
@@ -23,43 +23,4 @@ public class DeletedEmployee {
 
     private long phoneNo;
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public long getPhoneNo() {
-        return phoneNo;
-    }
-
-    public void setPhoneNo(long phoneNo) {
-        this.phoneNo = phoneNo;
-    }
 }
--- src/main/java/com/employees/application/model/User.java
+++ src/main/java/com/employees/application/model/User.java
@@ -22,35 +22,4 @@ public class User {
 
     private String roles;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getRoles() {
-        return roles;
-    }
-
-    public void setRoles(String roles) {
-        this.roles = roles;
-    }
 }
